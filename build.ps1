76e6518939fd3a75777d334fd6500a6d374b2ee3
Dieter Verfaillie <dieter.verfaillie@imog.be>
2021-01-04 09:28:44 +0100

    Cake.Frosting ToolInstaller working directory example
    An example using Cake.Frosting 1.0.0-rc0002 which shows
    the `ToolInstaller` still uses a hardcoded
    `_environment.WorkingDirectory.Combine("tools").MakeAbsolute(_environment);`
    path to install tools into.
    
    
    Observe that CakeHost() was configured with a .UseWorkingDirectory("../..")
    but surprisingly the tools end up being installed in `./build/source/tools`
    instead of the expected `./tools` directory as was the case
    in Cake.Frosting 1.0.0-rc0001.
    
    Now, as far as the `ToolInstaller` is concerned, both are wrong in this case.
    The [Paths][Tools] setting from the cake.config file has been specified and
    should be used instead.
    
    See https://github.com/cake-build/cake/issues/2904#issuecomment-753523941

--- build.ps1
+++ build.ps1
@@ -1,2 +1,69 @@
-dotnet run --project build/Build.csproj -- $args
-exit $LASTEXITCODE;
+# Script root
+$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+# Read configuration file
+function Get-IniContent ($filePath)
+{
+    $ini = @{}
+    switch -regex -file $FilePath
+    {
+        "^\[(.+)\]$" # Section
+        {
+            $section = $matches[1]
+            $ini[$section] = @{}
+            $CommentCount = 0
+        }
+        "^(;.*)$" # Comment
+        {
+            if (!($section))
+            {
+                $section = "No-Section"
+                $ini[$section] = @{}
+            }
+            $value = $matches[1]
+            $CommentCount = $CommentCount + 1
+            $name = "Comment" + $CommentCount
+            $ini[$section][$name] = $value
+        }
+        "(.+?)\s*=\s*(.*)" # Key
+        {
+            if (!($section))
+            {
+                $section = "No-Section"
+                $ini[$section] = @{}
+            }
+            $name,$value = $matches[1..2]
+            $ini[$section][$name] = $value
+        }
+    }
+    return $ini
+}
+
+$buildConfig = Get-IniContent(Join-Path $PSScriptRoot "cake.config")
+
+# Make sure tools folder exists
+$ToolPath =  $buildConfig["Paths"]["Tools"]
+if (!(Test-Path $ToolPath)) {
+    Write-Verbose "Creating tools directory..."
+    New-Item -Path $ToolPath -Type directory | out-null
+}
+
+Push-Location
+Set-Location build\source
+
+Write-Host "Preparing Cake.Frosting build runner..."
+Invoke-Expression "dotnet restore"
+if($LASTEXITCODE -ne 0) {
+    Pop-Location;
+    exit $LASTEXITCODE;
+}
+
+Write-Host "Running Cake.Frosting build runner..."
+Write-Host "dotnet run -- $args"
+Invoke-Expression "dotnet run -- $args"
+if($LASTEXITCODE -ne 0) {
+    Pop-Location;
+    exit $LASTEXITCODE;
+}
+
+Pop-Location
--- build/source/Program.cs
+++ build/source/Program.cs
@@ -1,11 +1,11 @@
-using System.Threading.Tasks;
+using System;
+
 using Cake.Common;
 using Cake.Common.IO;
 using Cake.Common.Tools.DotNetCore;
 using Cake.Common.Tools.DotNetCore.Build;
 using Cake.Common.Tools.DotNetCore.Test;
 using Cake.Core;
-using Cake.Core.Diagnostics;
 using Cake.Frosting;
 
 public static class Program
@@ -13,8 +13,10 @@ public static class Program
     public static int Main(string[] args)
     {
         return new CakeHost()
+            .UseWorkingDirectory("../..")
             .UseContext<BuildContext>()
-            .UseWorkingDirectory("..")
+            .UseTool(new Uri("nuget:?package=ReportGenerator&version=4.8.1"))
+            .UseTool(new Uri("nuget:?package=NuGet.CommandLine&version=5.8.0"))
             .Run(args);
     }
 }
@@ -27,14 +29,15 @@ public class BuildContext : FrostingContext
         : base(context)
     {
         MsBuildConfiguration = context.Argument("configuration", "Release");
-    }}
+    }
+}
 
 [TaskName("Clean")]
 public sealed class CleanTask : FrostingTask<BuildContext>
 {
     public override void Run(BuildContext context)
     {
-        context.CleanDirectory($"./src/Example/bin/{context.MsBuildConfiguration}");
+        context.CleanDirectory($"./source/Example/bin/{context.MsBuildConfiguration}");
     }
 }
 
@@ -44,7 +47,7 @@ public sealed class BuildTask : FrostingTask<BuildContext>
 {
     public override void Run(BuildContext context)
     {
-        context.DotNetCoreBuild("./src/Example.sln", new DotNetCoreBuildSettings
+        context.DotNetCoreBuild("./source/Example.sln", new DotNetCoreBuildSettings
         {
             Configuration = context.MsBuildConfiguration,
         });
@@ -57,7 +60,7 @@ public sealed class TestTask : FrostingTask<BuildContext>
 {
     public override void Run(BuildContext context)
     {
-        context.DotNetCoreTest("./src/Example.sln", new DotNetCoreTestSettings
+        context.DotNetCoreTest("./source/Example.sln", new DotNetCoreTestSettings
         {
             Configuration = context.MsBuildConfiguration,
             NoBuild = true,
